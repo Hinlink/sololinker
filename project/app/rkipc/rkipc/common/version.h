6cd7f6ebcd50a814b9a7994c66f0062647ac6e52
xianlee.wu <xianleewu@163.com>
2023-11-28 09:51:52 +0000

    fix app
    Signed-off-by: xianlee.wu <xianleewu@163.com>

---
+++ project/app/rkipc/rkipc/common/version.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2023 Rockchip Electronics Co. LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef SRC_VERSION_H_
+#define SRC_VERSION_H_
+
+#define RKIPC_VERSION_INFO        "unknown rkipc version for missing VCS info"
+#define RKIPC_BUILD_INFO          "unknown rkipc build info"
+#define RKIPC_VER_HIST_CNT        0
+#define RKIPC_VER_HIST_0          NULL
+#define RKIPC_VER_HIST_1          NULL
+#define RKIPC_VER_HIST_2          NULL
+#define RKIPC_VER_HIST_3          NULL
+#define RKIPC_VER_HIST_4          NULL
+#define RKIPC_VER_HIST_5          NULL
+
+#define RKIPC_TYPE                "COMPILE_FOR_RV1106_IPC"
+
+#endif /*SRC_VERSION_H_*/
